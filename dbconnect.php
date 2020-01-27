ca4297952f80d250333a7b04267efc20f85b6dec
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 19:49:30 +0800

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $mysqli = new mysqli("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820680");
-
-if ($mysqli->connect_errno) {
-  echo "Connect Failed ".$mysqli->connect_error;
-  
-  exit();
-}
-?>
