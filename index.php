5f57ad5f8dc12e8ee7a4a297b9dc266a0abcce5f
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 23:07:23 +0800

    Update and rename getXML.php to index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    $rss= '<?xml version="1.0" encoding="UTF-8"?>';
+    $rss .= '<rss version="2.0">';
+    $rss .= '<channel>';
+
+    $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820680") or die (mysqli_error($connect));
+    $sql = "SELECT * FROM tbl_articles";
+    $query = mysqli_query($connect, $sql) or die (mysqli_error($connect));
+
+    while($result= mysqli_fetch_assoc($query)){
+        extract($result);
+        
+        $rss .= '<articles>';
+        $rss .= '<title>' . $article_title . '</title>';
+        $rss .= '<description>' . $description . '</description>';
+        $rss .= '<author>' . $author . '</author>';
+        $rss .= '<created>' . $date_created . '</created>';
+        $rss .= '</articles>';
+    }
+    $rss .= '</channel>';
+    $rss .= '</rss>';
+
+    echo $rss;  
+?>
+
+
+
+
