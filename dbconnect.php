76268b2234c046312cfcc76e77e2f0ddccaf1598
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 18:32:42 +0800

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+  date_default_timezone_set("Asia/Manila");
+  define('DB_SERVER','localhost');
+  define('DB_USERNAME','root');
+  define('DB_PASSWORD','');
+  define('DB_DATABASE','db_waters');
+  session_start();
+?>
