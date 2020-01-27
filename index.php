9a4f178e9e33cba0da259b68d9ed0c16e09a471f
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 22:25:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ header("Content-Type: application/rss+xml; charset=ISO-8859-1");
   $rssfeed .= '<?xml version="1.0" encoding="ISO-8859-1"?>';
   $rssfeed .= '<rss version="2.0"?>';
   $rssfeed .= '<channel>';
-  
-  $sql = "SELECT * FROM tbl_articles";
+ 
   $connect = mysqli_connect("http://dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234","db_1820680") or die(mysqli_error($connect));
+  $sql = "SELECT * FROM tbl_articles";
   $query = mysqli_query($connect, $sql) or die(mysqli_error($connect));
   while($row=mysqli_fetch_assoc($query)) {
     extract($row);
