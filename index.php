96e17bf8fd285d2e8c52ceb95c62afdf6586370b
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 21:07:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+  
+  $rssfeed .= '<?xml version="1.0" encoding="ISO-8859-1"?>';
+  $rssfeed .= '<rss version="2.0"?>';
+  $rssfeed .= '<channel>';
+  
+  $sql = "SELECT * FROM tbl_articles";
+  $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234") or die(mysqli_error($connect));
+  $db = mysqli_select_db($connect,"db_1820680");
+  $query = mysqli_query($connect, $sql) or die(mysqli_error($connect));
+  while($row=mysqli_fetch_assoc($query)) {
+    extract($row);
+    
+    $rssfeed .= '<articles>';
+    $rssfeed .= '<title>' . $article_title . '</title>';
+    $rssfeed .= '<description>' . $description . '</description>';
+    $rssfeed .= '<author>' . $author . '</author>';
+    $rssfeed .= '<date_created>' . $date_created . '</date_created';
+    $rssfeed .= '</articles>';
+  }
+  
+  $rssfeed .= '</channel>';
+  $rssfeed .= '</rss>';
+?>
