e1732d1ed15d060d3a6ac5a2b67006b4eafda39c
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 20:04:53 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,8 +2,8 @@ CREATE DATABASE db_1820680;
 
 CREATE TABLE `tbl_articles` (
   `article_id` int(5) unsigned NOT NULL auto_increment, 
-  `article_title` VARCHAR(64) NOT NULL DEFAULT '',
-  `description` VARCHAR(64) NOT NULL DEFAULT '',
+  `article_title` VARCHAR(100) NOT NULL DEFAULT '',
+  `description` VARCHAR(60000) NOT NULL DEFAULT '',
   `author` VARCHAR(64) NOT NULL DEFAULT '',
   `date_created` DATETIME NULL,
   PRIMARY KEY  (`article_id`)
@@ -11,3 +11,15 @@ CREATE TABLE `tbl_articles` (
 
 INSERT INTO tbl_articles (article_title, description, author, date_created)
 VALUES ('Coronavirus Infections—More Than Just the Common Cold','Hello World','Catharine I. Paules, MD',NOW());
+INSERT INTO tbl_articles (article_title, description, author, date_created)
+VALUES ('Coronavirus death toll jumps above 80, five US cases confirmed: Everything we know',
+		  'The new coronavirus, which has now claimed more than 80 lives in China, has now been identified in Arizona, California, Illinois and Washington state.',
+		  'Jackson Ryan',NOW());
+INSERT INTO tbl_articles (article_title, description, author, date_created)
+VALUES ('How the new coronavirus stacks up against SARS and MERS',
+		  'Coronaviruses, one of a variety of viruses that cause colds, have been making people cough and sneeze seemingly forever. But occasionally, a new version infects people and causes serious illness and deaths.',
+		  'Tina Hesman Saey',NOW());
+INSERT INTO tbl_articles (article_title, description, author, date_created)
+VALUES ('Snakes Could Be the Original Source of the New Coronavirus Outbreak in China',
+		  'A study of the virus’s genetic sequence suggests similarities to that seen in snakes, but the origin must still be verified.',
+		  'Haitao Guo',NOW());
