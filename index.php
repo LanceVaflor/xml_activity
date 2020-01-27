431b3474d2c9add1004c7dd2057b3ae39f03459f
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-27 21:28:30 +0800

    Update and rename test.php to index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-  $mysqli = new mysqli("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820680");
-
-  if ($mysqli->connect_errno) {
-    echo "Connect Failed ".$mysqli->connect_error;
-    exit();
-  }
+  $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com","admin","root1234");
+  $db = mysqli_select_db($connect, "db_1820680");
   
   $query_database = "SELECT article_id, article_title, description, author, date_created FROM tbl_articles";
   $articlesArray = array();
