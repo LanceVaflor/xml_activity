ae906793750836c1c6bf9b5aaa7538cffa6b965f
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-28 13:57:48 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -4,7 +4,7 @@
     $rss .= '<channel>';
 
     $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820680") or die (mysqli_error($connect));
-    $sql = "SELECT * FROM tbl_articles";
+    $sql = "SELECT * FROM tbl_articles ORDER BY article_id ASC;";
     $query = mysqli_query($connect, $sql) or die (mysqli_error($connect));
 
     while($result= mysqli_fetch_assoc($query)){
