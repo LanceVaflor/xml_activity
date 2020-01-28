f60da4a950a66a21a310b7001660195a4dd3d724
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-28 13:55:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
 <?php
-  $domOBJ = new DOMDocument();
-  $domOBJ->load("https://xmlactivity.herokuapp.com/rss.php");//XML page URL
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmlactivity.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("shoes");
+ 
+ ?>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("article_title")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+   $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+   $created = $data->getElementsByTagName("created")->item(0)->nodeValue;
+   
   
-  $content = $domOBJ->getElementsByTagName("articles");
+   echo "<li>$title
+            <ul>
+                <li>Content: $description</li>
+		<li>Author: $author</li>
+		<li>Date Created: $created</li>
+            </ul>
+        </li>";
+ }
 ?>
-
-<h1> ARTICLES </h1>
-<table>
-  <tr>
-      <td> Article Title </td>
-      <td> Content </td>
-      <td> Author </td>
-      <td> Date Created </td>
-  </tr>
-<?php
-  foreach( $content as $data )
-  {
-     $title = $data->getElementsByTagName("article_title")->item(0)->nodeValue;
-     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-     $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
-     $created = $data->getElementsByTagName("created")->item(0)->nodeValue;
-    
-  echo "<tr>
-            <td> $title </td>
-            <td> $description  </td>
-            <td> $author </td>
-            <td> $created </td>
-        </tr> "
-  }
-?>
-</table>
+</ul>
   
