49f5aee69dd6f494a26ef4d2960b8785ffe566b5
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-30 17:33:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
  $domOBJ->load("https://xmlactivity.herokuapp.com/rss.php");//XML page URL
  
  $content = $domOBJ->getElementsByTagName("articles");
- 
  ?>
- <ul>
-    <?php
+
+ <h1>Articles</h1>
+
+ <?php
  foreach( $content as $data )
  {
-   $title = $data->getElementsByTagName("article_title")->item(0)->nodeValue;
-   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
-   $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
-   $created = $data->getElementsByTagName("created")->item(0)->nodeValue;
-  
-   echo "<li>$title
-            <ul>
-                <li>Content: $description</li>
-		<li>Author: $author</li>
-		<li>Date Created: $created</li>
-            </ul>
-        </li>";
+     $title = $data->getElementsByTagName("article_title")->item(0)->nodeValue;
+     $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+     $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+     $created = $data->getElementsByTagName("created")->item(0)->nodeValue;
+     echo "<ul>
+            <h2>$title</h2>
+              <ul>
+                  <li>Description: $description </li>
+                  <li>Author: $author </li>
+                  <li>Date Created: $created </li>
+              </ul>
+          </ul>";
  }
 ?>
-</ul>
   
